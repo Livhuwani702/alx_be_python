8ab1ee1d82304a3151bfbe2a0c03266957c1362b
Livhuwani Caroline Muntswu <muntswulivhucarol@gmail.com>
2025-09-21 17:16:12 +0200

    added basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number2 - number1
+product = number1 * number2
+print(f"sum of {number1} and {number2} is {sum}")
+print(f"difference of {number1} and {number2} is {difference}")
+print(f"product of {number1} and {number2} is {product}")
