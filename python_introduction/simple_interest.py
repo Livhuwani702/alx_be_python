cbc314bb60c2b986f45a132e9c7ad30368728bc4
Livhuwani <muntswulivhucarol@gmail.com>
2025-10-23 12:41:53 +0200

    Rearrange code for calculating simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,8 @@
-principle = $1000
+principle = 1000
+print(f"principle: ${principle}")
 rate = 0.05
 time = 3
 interest = principle * rate * time
-print(f"principle: ${principle}")
 print(f"rate: {rate*100}%")
 print(f"time: {time} years")
 print(f"The simple interest is: {interest}")
-
