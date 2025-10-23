dcc8d57aabec01a49d14647acb2c17adc00fe6ac
Livhuwani <muntswulivhucarol@gmail.com>
2025-10-23 12:34:35 +0200

    Fix formatting of simple interest calculation

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,4 +1,4 @@
-principle = 1000
+principle = $1000
 rate = 0.05
 time = 3
 interest = principle * rate * time
@@ -6,3 +6,4 @@ print(f"principle: ${principle}")
 print(f"rate: {rate*100}%")
 print(f"time: {time} years")
 print(f"The simple interest is: {interest}")
+
