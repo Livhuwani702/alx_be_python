a121ba4feacd2857ec46dc48c8a5ac5f48a94a24
Livhuwani <muntswulivhucarol@gmail.com>
2025-10-23 12:14:58 +0200

    Enhance simple interest calculation output
    Added print statements to display principle, rate, and time.

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,7 @@ principle = 1000
 rate = 0.05
 time = 3
 interest = principle * rate * time
-
+print(f"principle: ${principle}")
+print(f"rate: {rate*100}%")
+print(f"time: {time} years")
 print(f"The simple interest is: {interest}")
