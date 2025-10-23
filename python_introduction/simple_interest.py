4fca5f536d0e7e8c62a033b317093aafbbd02269
Livhuwani <muntswulivhucarol@gmail.com>
2025-10-23 12:55:15 +0200

    Fix variable name from 'principle' to 'principal'

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,9 @@
-principle = 1000
+principal = 1000 
 rate = 0.05
 time = 3
 interest = principle * rate * time
 
-print(f"principle: ${principle}")
+print(f"principle: ${principal}")
 print(f"rate: {rate*100:.0f}%")
 print(f"time: {time} years")
 print(f"The simple interest is: {interest}")
