3e47a7566c5e3e1fcb18bb3e28a9d972ed359aaf
Livhuwani <muntswulivhucarol@gmail.com>
2025-10-23 12:48:24 +0200

    Rearrange print statements for clarity

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,9 @@
 principle = 1000
-print(f"principle: ${principle}")
 rate = 0.05
 time = 3
 interest = principle * rate * time
-print(f"rate: {rate*100}%")
+
+print(f"principle: ${principle}")
+print(f"rate: {rate*100:.0f}%")
 print(f"time: {time} years")
 print(f"The simple interest is: {interest}")
