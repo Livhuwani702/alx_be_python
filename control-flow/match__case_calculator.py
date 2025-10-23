d8655599c6a05b91ea6d0c498afbf048d1213fa5
Livhuwani <muntswulivhucarol@gmail.com>
2025-10-23 14:35:30 +0200

    Fix syntax errors in match case calculator

--- control-flow/match__case_calculator.py
+++ control-flow/match__case_calculator.py
@@ -2,13 +2,13 @@ num1 = float(input("Enter the first number:")
 num2 = float(input("Enter the second number:")
 operation = input("Choose the operation (+, -, *, /):")
 match operation:
-case "+": 
-result = num1 + num2
-case "-":
-result = num1 - num2
-case "*":
-result = num1 * num2
-case "/":
-result = num1 / num2
-if num2 = 0:
-print("Cannot divide by zero.")
+  case "+": 
+    result = num1 + num2
+  case "-":
+    result = num1 - num2
+  case "*":
+    result = num1 * num2
+  case "/":
+    result = num1 / num2
+    if num2 = 0:
+    print("Cannot divide by zero.")
