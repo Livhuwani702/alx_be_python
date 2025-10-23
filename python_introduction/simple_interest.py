af5a5fc4e27548203bc70c7487abc7b4ce0c2b7b
Livhuwani <muntswulivhucarol@gmail.com>
2025-10-23 12:55:56 +0200

    Fix variable name typo in simple interest calculation

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,10 @@
 principal = 1000 
 rate = 0.05
 time = 3
-interest = principle * rate * time
+interest = principal * rate * time
 
 print(f"principle: ${principal}")
 print(f"rate: {rate*100:.0f}%")
 print(f"time: {time} years")
 print(f"The simple interest is: {interest}")
+
